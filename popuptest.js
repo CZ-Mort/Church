7ac31ab385fca19fcd4ba3ce602710412f65be38
CZ-Mort <127400034+CZ-Mort@users.noreply.github.com>
2023-03-09 22:15:51 +1100

    the start...
    praise our lord and saviour jesus christ

---
+++ popuptest.js
@@ -0,0 +1,11 @@
+<!-- PopMyAds.com Popunder Code for saintignatius500years.neocities.org -->
+<head>
+<script type="text/javascript" data-cfasync="false">
+var pmauid = '66757';
+var pmawid = '70638';
+var fq = '0';
+</script>
+<script type="text/javascript" data-cfasync="false" src="https://cdn.popmyads.com/pma.js"></script>
+ 
+<!-- PopMyAds.com Popunder Code End -->
+</head>
---
+++ test.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+   var uid = '338263';
+   var wid = '640197';
+   var pop_tag = document.createElement('script');pop_tag.src='//cdn.popcash.net/show.js';document.body.appendChild(pop_tag);
+   pop_tag.onerror = function() {pop_tag = document.createElement('script');pop_tag.src='//cdn2.popcash.net/show.js';document.body.appendChild(pop_tag)};
+</script>
